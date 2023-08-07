3824664c9c43c95f8b7f55d3e11b878d9837ac0f
hazem-hg <62479573+hazem-hg@users.noreply.github.com>
2023-08-07 17:03:14 +0100

    first solution

---
+++ DEMO/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DEMO;
+
+var input = new int[] {0, 1, 8, 7, 4, 0, 2, 6};
+var n = 8;
+Solution.findMissingNumbers(n,input); 
---
+++ DEMO/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace DEMO;
+
+public static class Solution
+{
+    public static  void findMissingNumbers(int n, int[] t)
+    {
+        // using the hashset. so checking uf element is in collection will be O(1)
+        HashSet<int> numbersHashSet = new HashSet<int>(t);
+        for (int i = 1; i <= n; i++)
+        {
+            if (!numbersHashSet.Contains(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+
+   
+}
